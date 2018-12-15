dd9d3aed6633bbf03c2370cf8fb3f0dec9885d99
VadymMatiash <vadym.matiash@gmail.com>
2018-12-15 14:55:07 +0200

    rewrited server for deployment and configured docker

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const path = require('path');
 const gitVersion = require('git-tag-version');
 const app = express();
 const port = 5000;
 const tag = gitVersion();
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'client/build/index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}, version: ${tag}`);
